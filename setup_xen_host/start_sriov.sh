c1c2a7032e696f3f3ac2b11ff809ad4ecd4cd8d9
Jason Anderson <jwa2@clemson.edu>
2016-03-16 10:00:17 -0400

    stuff

---
+++ setup_xen_host/start_sriov.sh
@@ -0,0 +1,11 @@
+num_vfs=$1
+
+iface=`ifconfig | grep 00:8c:fa | awk '{print $1}'`
+
+#../tools/add_sriov_devs $iface $num_vfs
+
+sriov_ifaces=`ifconfig -a | grep sriov | awk '{print $1}'`
+for if in $ifaces; do
+	dev_name=`ethtool -i $if | grep bus-info | awk '{print $2}'`
+	echo $dev_name
+done
