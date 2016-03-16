b255fc595611243d7c398c3813b605dda7c72729
rakurai <rakurai@xen.rakurai-qv14169.bdslab-pg0.clemson.cloudlab.us>
2016-03-16 10:18:26 -0400

    b

--- setup_xen_host/start_sriov.sh
+++ setup_xen_host/start_sriov.sh
@@ -5,7 +5,7 @@ iface=`ifconfig | grep 00:8c:fa | awk '{print $1}'`
 #../tools/add_sriov_devs $iface $num_vfs
 
 sriov_ifaces=`ifconfig -a | grep sriov | awk '{print $1}'`
-for if in $ifaces; do
+for if in $sriov_ifaces; do
 	dev_name=`ethtool -i $if | grep bus-info | awk '{print $2}'`
-	echo $dev_name
+	xl pci-assignable-add $dev_name
 done
